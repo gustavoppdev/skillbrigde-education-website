4de48de6f3d51b02225ac95e9c8a24478aa564d3
Gustavo Henrique <gustavopp.dev@gmail.com>
2025-12-13 15:17:23 -0300

    refactor: muda e organiza a estrutura de pastas

--- src/app/[locale]/(home)/components/OurCourses/OurCourses.tsx
+++ src/app/[locale]/(home)/components/OurCourses/OurCourses.tsx
@@ -1,19 +1,21 @@
+// Components
 import SectionTitle from "@/components/common/SectionTitle";
-import { TestimonialsArray } from "@/constants";
+import CourseCard from "./CourseCard";
 
-import TestimonialCard from "./TestimonialCard";
+// Constants
+import { Courses } from "@/constants";
 
-export const Testimonials = () => {
+export const OurCourses = () => {
   return (
     <section className="section-container mt-15 lg:mt-25 2xl:mt-37.5">
       <SectionTitle
-        translationNamespace="Pages.Home.Testimonials"
+        translationNamespace="Pages.Home.OurCourses"
         viewMore={true}
+        viewMoreUrl="/courses"
       />
-
       <div className="mt-10 lg:mt-15 2xl:mt-20 grid grid-cols-1 md:grid-cols-2 gap-5">
-        {TestimonialsArray.map((testimony) => (
-          <TestimonialCard key={testimony.author} testimony={testimony} />
+        {Courses.map((course) => (
+          <CourseCard key={course.title} course={course} />
         ))}
       </div>
     </section>
---
+++ src/app/[locale]/(home)/page.tsx
@@ -0,0 +1,23 @@
+import { Hero } from "@/app/[locale]/(home)/components/Hero";
+import VideoSection from "@/app/[locale]/(home)/components/VideoSection";
+import { Benefits } from "@/app/[locale]/(home)/components/Benefits";
+import { OurCourses } from "@/app/[locale]/(home)/components/OurCourses";
+import { Testimonials } from "./components/Testimonials";
+import { FAQ } from "@/components/sections/FAQ";
+import { Pricing } from "@/components/sections/Pricing";
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <VideoSection />
+      <Benefits />
+      <OurCourses />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+    </main>
+  );
+};
+
+export default Home;
